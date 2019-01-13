8c4f57d98efb9ed4927906ecbfd785edce4a9e43
Ivo Janssen <jansseni@amazon.com>
2019-01-13 11:32:04 -0600

    init database when bastion boots (not the best way to init a cf stack, todo to rework this). will fail if db already exists, so bastion respawns won't affect existing db's

---
+++ assets/octank.sql
@@ -0,0 +1,16 @@
+--DROP TABLE customers;
+CREATE TABLE customers (
+  customer_id INT NOT NULL,
+  customer_name VARCHAR(200) NOT NULL,
+  industry VARCHAR(200) NOT NULL,
+  market_cap VARCHAR(200) NOT NULL,
+  PRIMARY KEY (customer_id)
+);
+
+INSERT INTO customers
+    (customer_id, customer_name, industry, market_cap)
+VALUES
+    (1,"Amazon", "Retail", "$820B"),
+    (2,"Wells Fargo", "Financial", "$225B"),
+    (3,"Tesla", "Automotive", "$60B");
+
